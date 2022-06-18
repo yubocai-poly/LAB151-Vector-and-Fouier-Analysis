1f3c4f0e5a899a73b60169d478edc1d8e384f5f4
Yubo Cai <98619371+yubocai-poly@users.noreply.github.com>
2022-06-19 01:58:20 +0200

    Add files via upload

---
+++ fouier prove.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# plt.style.use("ggplot")
+# Setup
+x_ = np.linspace(-3,3,10000)
+
+T = np.pi*2
+Harmonics = 500
+
+def squareWave(x):
+    global T
+    lowerBoundLeft = (-T/2)
+    lowerBoundRight = 0
+    upperBoundLeft = 0
+    upperBoundRight = (T/2)
+    one = 1
+    negativeOne = -1
+        
+    while True:
+        if (x >= lowerBoundLeft) and (x <= lowerBoundRight):
+            return negativeOne
+        elif (x >= upperBoundLeft) and (x <= upperBoundRight):
+            return one
+        else:
+            lowerBoundLeft -= T/2
+            lowerBoundRight -= T/2
+            upperBoundLeft += T/2
+            upperBoundRight += T/2
+            if one == 1:
+                one = -1
+                negativeOne = 1
+            else:
+                one = 1
+                negativeOne = -1
+                
+# Bn coefficients
+def bn(n):
+    n = int(n)
+    if (n%2 != 0):
+        return 4/(np.pi*n)
+    else:
+        return 0
+# Wn
+def wn(n):
+    global T
+    wn = (2*np.pi*n)/T
+    return wn
+
+# Fourier Series function
+def fourierSeries(n_max,x):
+    a0 = 0
+    partialSums = a0
+    for n in range(1,n_max):
+        try:
+            partialSums = partialSums + bn(n)*np.sin(wn(n)*x)
+        except:
+            print("pass")
+            pass
+    return partialSums
+
+y = []
+f = []
+for i in x_:
+    y.append(squareWave(i))
+    f.append(fourierSeries(Harmonics,i))
+    
+fig = plt.figure(figsize=(13, 10))
+plt.plot(x_,y,color="blue",label="Signal")
+plt.plot(x_,f,color="red",label="Fourier series approximation")
+plt.title("Fourier Series approximation number of Harmonics: "+str(Harmonics))
+plt.legend()
+plt.show()
